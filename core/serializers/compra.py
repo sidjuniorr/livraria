0fd5c05ecefe39ffa6102047e0f512e1e3e458dc
sidjuniorr <sidjunior111@gmail.com>
2024-12-04 20:22:57 -0300

    Inclusão do email do usuário na listagem da compra

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.models import Compra
+from rest_framework.serializers import CharField, ModelSerializer
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True) # inclua essa linha
     class Meta:
         model = Compra
         fields = "__all__"
+    
