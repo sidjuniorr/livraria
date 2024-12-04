3d8a9ac6e479c1d962be9900032ddceb6794a620
sidjuniorr <sidjunior111@gmail.com>
2024-12-04 20:07:17 -0300

    Criação da entidade Compra integrada ao usuário do projeto

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from core.models import Autor, Categoria, Editora, Livro, User
+from core.models import Compra
 
 
 @admin.register(User)
@@ -86,3 +87,5 @@ class LivroAdmin(admin.ModelAdmin):
     list_filter = ("editora", "categoria")
     ordering = ("titulo", "editora", "categoria")
     list_per_page = 25
+    
+admin.site.register(Compra)
